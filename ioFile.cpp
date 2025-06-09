080ab3b55a577bf3703e12dc3931e41367a3df28
devitadwiindayani <devitadwiindayani@gmail.com>
2025-06-09 14:40:29 +0700

    add membuat objek dan membuka file

--- ioFile.cpp
+++ ioFile.cpp
@@ -7,3 +7,10 @@ int main()
 {
     string baris;
 
+    // membuat objek output file stream.
+    ofstream outfile;
+    // membuka file untuk ditulisi.
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
